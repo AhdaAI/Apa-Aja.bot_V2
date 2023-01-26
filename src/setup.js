92c1ac15a4bd0b988ce80e7dd0da66be6f7c9e6e
Ahda Akmalul Ilmi <86042310+RazorHex@users.noreply.github.com>
2023-01-27 00:25:19 +0700

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -37,7 +37,7 @@ module.exports = {
         .setDescription("Add short description to your server.")
         .setRequired(false)
     ),
-  test: true,
+  test: false,
   /**
    *
    * @param {import("discord.js").Interaction} interaction
